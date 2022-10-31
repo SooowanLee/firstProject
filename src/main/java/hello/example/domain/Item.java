559684a23d817b74622c4403c971ec29354001f0
sooowanlee <lsy9406@gmail.com>
2022-10-31 14:51:27 +0900

    feat ItemEntity DB설정
    notNull, length, id 등 설정

--- src/main/java/hello/example/domain/Item.java
+++ src/main/java/hello/example/domain/Item.java
@@ -4,24 +4,37 @@ import hello.example.constant.ItemSellStatus;
 import lombok.Getter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @ToString
+@Table(name = "item")
 public class Item {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "item_id")
     private Long id;                        //상품 코드
+
+    @Column(nullable = false, length = 55)
     private String itemName;                //상품 명
+
+    @Column(nullable = false, name = "price")
     private int price;                      //상품 가격
+
+    @Column(nullable = false)
     private int stockNumber;                //재고 수량
+
+    @Lob
+    @Column(nullable = false)
     private String itemDetail;              //상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;  //상품 판매 상태
+
     private LocalDateTime regTime;          //등록 시간
+
     private LocalDateTime updateTime;       //수정 시간
 }
 
