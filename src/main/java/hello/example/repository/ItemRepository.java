6396356dd4d06199d51a25a20e445834b70937d6
sooowanlee <lsy9406@gmail.com>
2022-11-09 21:06:18 +0900

    add 상품상세설명을 파라미터로 받는 검색기능
     itemRepository에 findByItemDetail을 추가했다

--- src/main/java/hello/example/repository/ItemRepository.java
+++ src/main/java/hello/example/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package hello.example.repository;
 
 import hello.example.domain.Item;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public interface ItemRepository {
     List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
 
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
     List<Item> findAll();
 }
