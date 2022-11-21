7e69dd71371e662bd3062f64f044942893e8b2c2
sooowanlee <lsy9406@gmail.com>
2022-11-21 20:40:54 +0900

    fix MemberFormDto 어노테이션 변경
    email, password, address @NotBlank -> @NotEmpty

--- src/main/java/hello/example/dto/MemberFormDto.java
+++ src/main/java/hello/example/dto/MemberFormDto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 @Getter @Setter
@@ -13,15 +14,15 @@ public class MemberFormDto {
     @NotBlank(message = "필수 정보입니다.")
     private String name;
 
-    @NotBlank(message = "필수 정보입니다.")
+    @NotEmpty(message = "필수 정보입니다.")
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
-    @NotBlank(message = "필수 정보입니다.")
+    @NotEmpty(message = "필수 정보입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
             message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password;
 
-    @NotBlank(message = "필수 정보입니다.")
+    @NotEmpty(message = "필수 정보입니다.")
     private String address;
 }
